cdba5c241cce642b21a48931f12451d35f938efc
Michau <72571650+FNMikey@users.noreply.github.com>
2024-03-17 13:47:14 +0100

    Creating PlayerBehaviour
    Commit implements Player movement

---
+++ My project/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+
+    public float speed;
+    public int health;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 4;
+        speed = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var x = Input.GetAxis("Horizontal") * speed;
+        var y = Input.GetAxis("Vertical") * speed;
+
+        var rb = GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+
+            rb.velocity = new Vector2(x, y);
+
+        }
+
+
+    }
+}
